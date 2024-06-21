be841e76c247fca30a6874ea8a70493479a2ba09
Matheus Freitas <bandeira.freitas2013@gmail.com>
2024-06-21 15:25:24 -0300

    Ajustes na promoção

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -120,7 +119,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Não existe peça a ser promovida");
 		} 
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Tipo inválido da promoção");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
